f15ff9d0ad1f1179226177aa90c97b4a978d1216
Konrad (PC) <konrad@guzek.uk>
2023-05-21 19:46:59 +0200

    add methods for getting vehicle txd and id audio

--- CornyFlakezPlugin2/Util.cs
+++ CornyFlakezPlugin2/Util.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Reflection;
 using System.Windows.Forms;
 using Rage;
 
@@ -9,10 +10,61 @@ namespace CornyFlakezPlugin2
   {
     private static Dictionary<Keys, DateTime?> trackedKeys = new Dictionary<Keys, DateTime?>();
 
+    private static readonly Assembly assembly = Assembly.GetExecutingAssembly();
+
+    // private const string VehiclePreviewsResourceName = "CornyFlakezPlugin2.Resources.vehiclePreviews.json";
+    private const string VehiclePreviewsResourceName = "vehiclePreviews.json";
+
     private static DateTime lastCheck;
 
+    private static readonly Dictionary<string, (string, int[])[]> identificationSpeeches = new Dictionary<string, (string, int[])[]>()
+    {
+      {"lspd", new (string, int[])[] {
+        ("S_M_Y_COP_01_BLACK_FULL_01", new int[] { 1,2,3 }),
+        ("S_M_Y_COP_01_BLACK_FULL_02", new int[] { 1 }),
+        ("S_M_Y_COP_01_WHITE_FULL_01", new int[] { 2 }),
+        ("S_M_Y_COP_01_WHITE_FULL_02", new int[] { 1,2,3 }),
+        ("S_M_Y_HWAYCOP_01_BLACK_FULL_02", new int[] { 1 }),
+        ("S_M_Y_HWAYCOP_01_WHITE_FULL_01", new int[] { 1,2 }),
+        ("S_M_Y_COP_01_BLACK_MINI_01", new int[] { 1,3 }),
+        ("S_M_Y_COP_01_BLACK_MINI_02", new int[] { 1,3 }),
+        ("S_M_Y_COP_01_BLACK_MINI_04", new int[] { 2 }),
+        ("S_M_Y_COP_01_WHITE_MINI_01", new int[] { 3 }),
+        ("S_M_Y_COP_01_WHITE_MINI_02", new int[] { 1,2,3 }),
+        ("S_M_Y_COP_01_WHITE_MINI_03", new int[] { 1,2,3 }),
+        ("S_M_Y_COP_01_WHITE_MINI_04", new int[] { 3 }),
+        }},
+      {"sahp", new (string, int[])[] {
+        ("S_M_Y_HWAYCOP_01_BLACK_FULL_01", new int[] { 1, 2, 3, }),
+        ("S_M_Y_HWAYCOP_01_WHITE_FULL_02", new int[] { 2, 3, }),
+        }},
+      {"sheriff", new (string, int[])[] {
+        ("S_M_Y_SHERIFF_01_WHITE_FULL_01", new int[] { 1, 2, }),
+        ("S_M_Y_SHERIFF_01_WHITE_FULL_02", new int[] { 1, 2, }),
+        }},
+      {"nysp", new (string, int[])[] {
+        ("S_M_Y_MCOP_01_WHITE_MINI_01", new int[] { 1, }),
+        ("S_M_Y_MCOP_01_WHITE_MINI_02", new int[] { 1, }),
+        }},
+      {"generic", new (string, int[])[] {
+        ("S_M_Y_COP_01_BLACK_FULL_02", new int[] { 3 }),
+        ("S_M_Y_COP_01_WHITE_FULL_01", new int[] { 1,3 }),
+        ("S_M_Y_HWAYCOP_01_BLACK_FULL_02", new int[] { 2,3 }),
+        ("S_M_Y_HWAYCOP_01_WHITE_FULL_01", new int[] { 3 }),
+        ("S_M_Y_HWAYCOP_01_WHITE_FULL_02", new int[] { 1 }),
+        ("S_M_Y_COP_01_BLACK_MINI_01", new int[] { 2 }),
+        ("S_M_Y_COP_01_BLACK_MINI_02", new int[] { 2 }),
+        ("S_M_Y_COP_01_BLACK_MINI_03", new int[] { 1,2,3 }),
+        ("S_M_Y_COP_01_BLACK_MINI_04", new int[] { 1,3 }),
+        ("S_M_Y_COP_01_WHITE_MINI_01", new int[] { 1,2 }),
+        ("S_M_Y_MCOP_01_WHITE_MINI_02", new int[] { 1 }),
+        }},
+    };
+
+    static int[] a = { 1, 2, 3 };
+
     // Iterates through each tracked key, updating the timestamp it was pressed on.
-    private static void checkTrackedKeys()
+    private static void CheckTrackedKeys()
     {
       if (lastCheck == DateTime.Now) return;
       Dictionary<Keys, DateTime?> newValues = new Dictionary<Keys, DateTime?>();
@@ -35,7 +87,7 @@ namespace CornyFlakezPlugin2
 
     public static bool WasKeyHeld(Keys key, int durationMillis)
     {
-      checkTrackedKeys();
+      CheckTrackedKeys();
       DateTime? pressedAt;
       if (!trackedKeys.TryGetValue(key, out pressedAt))
       {
@@ -53,5 +105,57 @@ namespace CornyFlakezPlugin2
       TimeSpan duration = TimeSpan.FromMilliseconds(durationMillis);
       return pressedAt.Value + duration <= DateTime.Now;
     }
+
+
+    public static string GetVehicleTextureDictionary(string modelName)
+    {
+      VehicleTxdInfo vehicleTxdInfo;
+      if (!VehiclePreviews.VehicleTxdInfos.TryGetValue(modelName, out vehicleTxdInfo))
+      {
+        return null;
+
+      }
+      return vehicleTxdInfo.TextureDictionary;
+    }
+
+    public static void PlayIdentificationSpeech()
+    {
+      // TODO: Change me
+      // string agency = LSPD_First_Response.Mod.API.Functions.GetCurrentAgencyScriptName();
+      string agency = "lspd";
+      (string, int[])[] speeches;
+      bool usingSheriffSpeech = false;
+      identificationSpeeches.TryGetValue(agency, out speeches);
+      if (speeches == null)
+      {
+        switch (agency)
+        {
+          case "lssd":
+          case "bcso":
+            speeches = identificationSpeeches["sheriff"];
+            usingSheriffSpeech = true;
+            break;
+          default:
+            speeches = identificationSpeeches["generic"];
+            break;
+        }
+      }
+      int randomVoiceIdx = new Random().Next(speeches.Length);
+      (string, int[]) voiceInfo = speeches[randomVoiceIdx];
+      string voice = voiceInfo.Item1;
+      int randomSpeechVariantIdx = new Random().Next(voiceInfo.Item2.Length);
+      int speechVariant = voiceInfo.Item2[randomSpeechVariantIdx];
+      string speechName = usingSheriffSpeech ? "WAIT" : "COP_ARRIVAL_ANNOUNCE";
+      Game.LocalPlayer.Character.PlayAmbientSpeech(voice, speechName, speechVariant, SpeechModifier.Force);
+    }
+
+  }
+
+
+  public class VehicleTxdInfo
+  {
+    public string DisplayName { get; set; }
+    public string TextureDictionary { get; set; }
+    public string StorePageURL { get; set; }
   }
 }
